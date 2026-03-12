6edea626f73c0615a410b6b353c1d2dad85c23c3
Fesisp <mrfelipefss@gmail.com>
2026-03-11 20:44:55 -0400

    first commit

---
+++ my-daily-habits/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import BemVindo from "./components/BemVindo"
+
+function App() {
+  return (
+      <div>
+        <Header
+        titulo="My Daily Habits"
+        descricao="Construindo uma rotina melhor, um hábito por vez."
+        />
+        <BemVindo nomeUsuario="turma iteam" totalHabitos={777} />
+        <Footer />
+      </div>
+      
+  )
+}
+
+export default App
---
+++ my-daily-habits/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header ({ titulo, dsecricao }) {
+    return (
+        <div>
+            <h1>{titulo}</h1>
+            <p>{dsecricao}</p>
+        </div>
+    )
+}
+
+export default Header
